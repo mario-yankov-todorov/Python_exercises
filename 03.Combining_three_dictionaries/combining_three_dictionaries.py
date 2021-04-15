892fa13b824336b45278192fc00fd69b1bb66b27
Mario Todorov <mario.yankov.todorov@gmail.com>
2021-04-15 23:08:53 +0300

    Edit combining_three_dictionaries.py

---
+++ 03.Combining_three_dictionaries/combining_three_dictionaries.py
@@ -0,0 +1,42 @@
+# Merged three dictionaries
+if __name__ == '__main__':
+
+    Bulgarian_mountain_peaks_01 = {
+        'Рила'              :   'Мусала',
+        'Пирин'             :   'Вихрен',
+        'Стара планина'     :   'Ботев',
+        'Витоша'            :   'Черни връх',
+        'Осоговска планина' :   'Руен'
+    }
+
+    Bulgarian_mountain_peaks_02 = {
+        'Славянка'          :   'Гоцев връх',
+        'Родопи'            :   'Голям Перелик',
+        'Беласица'          :   'Радомир',
+        'Влахина планина'   :   'Кадийца',
+        'Малешевска планина':   'Ильов връх'
+    }
+
+    Bulgarian_mountain_peaks_03 = {
+        'Кървав камък'      :   'Било',
+        'Милевска'          :   'Милевец',
+        'Руй'               :   'Руй',
+        'Огражден'          :   'Билска чука',
+        'Средна гора'       :   'Богдан'
+    }
+
+    The_three_dictionaries_together = {
+        **Bulgarian_mountain_peaks_01,
+        **Bulgarian_mountain_peaks_02,
+        **Bulgarian_mountain_peaks_03
+    }
+
+    print('*** *** *** *** *** *** *** *** *** *** *** ***')     
+    for item in The_three_dictionaries_together.items():            
+        key, value = item
+        # To align the output data
+        n = 20 - len(key)
+        equal_sign = '-'*n
+        # Print output data
+        print(f'{key} {equal_sign}> {value}')
+    print('*** *** *** *** *** *** *** *** *** *** *** ***') 
