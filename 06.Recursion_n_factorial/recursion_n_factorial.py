04b783a52b54a1f156ac3dd9b7b8d913874e3b01
Mario Todorov <mario.yankov.todorov@gmail.com>
2021-04-27 10:58:42 +0300

    Edit recursion_n_factorial.py

--- 06.Recursion_n_factorial/recursion_n_factorial.py
+++ 06.Recursion_n_factorial/recursion_n_factorial.py
@@ -1,6 +1,18 @@
+def factorial(n):
+
+   if n == 1:
+       return n
+   else:
+       return n * factorial(n - 1)
 
 if __name__ == '__main__':
 
+    n = int(input('Please enter your number: '))
 
-
-    print()
+    if n < 0:
+        print('Sorry, factorial does not exist for negative numbers')
+    elif n == 0:
+        print('The factorial of 0 is 1')
+    else:
+        result = factorial(n)
+        print(f'The factorial of {n} is {result}')
